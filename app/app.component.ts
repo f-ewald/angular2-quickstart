06c96abda116ca14a0adc25fc160c39dfd72e6de
Friedrich Ewald <f-ewald@users.github.com>
2016-08-23 14:05:07 -0700

    added view

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
-    import { Component } from '@angular/core';
-    @Component({
-      selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
-    })
-    export class AppComponent { }
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/view/main/main.html'
+})
+export class AppComponent { }
